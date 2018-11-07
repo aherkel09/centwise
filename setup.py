4e3030f2e122b3fa4f39bb683b4f9ea0f24a1387
Avery Herkel <aherkel09@gmail.com>
2018-11-07 09:18:21 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,23 @@
 import sys
 from cx_Freeze import setup, Executable
 
-include_files = ['storeBudget.txt', 'overwrite.txt']
-build_exe_options = {"packages": ["os"], "includes": ["tkinter"],
-                     "include_files": include_files}
+include_files = ['storeBudget.txt', 'overwrite.txt', 'tcl86t.dll', 'tk86t.dll', 'centwise.py', 'osCommands.py']
+
+build_exe_options = {
+    "packages": ["os"],
+    "includes": ["tkinter"],
+    "include_files": include_files,
+}
 
 base = None
 if sys.platform == "win32":
     base = "Win32GUI"
 
-setup(  name = "Centwise",
-        version = "0.0.1",
-        description = "Generate Monthly Budgets",
-        options = {"build_exe": build_exe_options},
-        executables = [Executable("centwise.py", base=base)])
+setup(
+    name = "Centwise",
+    version = "0.0.1",
+    description = "Generate Monthly Budgets",
+    options = {"build_exe": build_exe_options},
+    executables = [Executable("centwise.py", base=base)],
+)
+
